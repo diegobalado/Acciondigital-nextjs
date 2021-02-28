4d30540a1d6e995d6c3f5d1c1a6a99c6d2ca9d19
Diego <jdiegomdq@gmail.com>
2021-02-28 20:11:50 -0300

    upgrades react and remove styled components

--- components/Header-TW/Nav.js
+++ components/Header-TW/Nav.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import { arrayOf, string, shape } from 'prop-types';
 // import styles from './Nav.module.scss';
-import styled from 'styled-components';
-
-const StyledNav = styled('nav')``;
-
-const StyledLinksGroup = styled.ul`
-    display: flex;
-`;
-
-const StyledLink = styled.li`
-  padding: 1.6rem;
-`;
-
-const StyledA = styled.a`
-  color: #ccc;
-  -webkit-transition: all 0.3s;
-  -moz-transition: all 0.3s;
-  transition: all 0.3s;
-`;
 
 const Nav = ({ items }) => (
   <nav>
     <ul className="flex">
       {items.map(({ href, icon, label }, index) => (
         <li key={`${index}-${label}`} className="p-6">
-            <StyledA href={href}>{label}</StyledA>
+            <a href={href}>{label}</a>
         </li>
       ))}
     </ul>
--- components/Header-TW/index.js
+++ components/Header-TW/index.js
@@ -4,7 +4,7 @@ import NavItems from './NavItems.json';
 // import logo from './logo.png';
 
 const Header = () => (
-  <div className="fixed flex items-center justify-between h-52 w-full">
+  <div className="fixed flex items-center justify-between h-52 w-full bg-gray-600">
 		<a href="/">
 			{/* <img src={logo} alt="Accion Digital"/> */}
 		</a>
